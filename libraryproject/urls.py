d9fd9541f2a371ac47422c66d5151cd4037c37a3
Roxanne Nasraty <rsnasraty@gmail.com>
2020-08-05 22:09:38 -0500

    Creating Library View

--- libraryproject/urls.py
+++ libraryproject/urls.py
@@ -21,4 +21,5 @@ from libraryapp.models import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('libraryapp.urls')),
+    path('librarians/', librarian_list, name='librarians'),
 ]
