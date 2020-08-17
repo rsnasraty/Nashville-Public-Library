1e70a31b0d3f930113e565ccd085a33ec1d5944c
Roxanne Nasraty <rsnasraty@gmail.com>
2020-08-16 21:15:25 -0500

    Finished thank god

--- libraryapp/views/books/form.py
+++ libraryapp/views/books/form.py
@@ -3,13 +3,14 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from libraryapp.models import Book
 from libraryapp.models import Library
-from libraryapp.models import model_factory
+# from libraryapp.models import model_factory
 from ..connection import Connection
 
 
 def get_libraries():
     with sqlite3.connect(Connection.db_path) as conn:
-        conn.row_factory = model_factory(Library)
+        conn.row_factory = sqlite3.Row
+        # conn.row_factory = model_factory(Library)
         db_cursor = conn.cursor()
 
         db_cursor.execute("""
