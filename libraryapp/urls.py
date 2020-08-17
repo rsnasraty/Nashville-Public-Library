9506f7956db01cee7b8de8c944f7490158d8d294
Roxanne Nasraty <rsnasraty@gmail.com>
2020-08-16 19:03:02 -0500

    saved?

---
+++ libraryapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import include, path
+from .views import *
+
+app_name = "libraryapp"
+
+    urlpatterns = [
+    path('', home, name='home'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('logout/', logout_user, name='logout'),
+    path('books/', book_list, name='books'),
+    path('libraries/', library_list, name='libraries'),
+    path('librarians/', librarian_list, name='librarians'),
+]
